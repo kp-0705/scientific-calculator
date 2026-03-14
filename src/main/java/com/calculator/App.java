4cdc83ba4c61d155e9102bd9c2ceada080a2c894
kp-0705 <kartavya0705@gmail.com>
2026-03-14 22:54:08 +0530

    Update App.java

--- src/main/java/com/calculator/App.java
+++ src/main/java/com/calculator/App.java
@@ -6,11 +6,11 @@ public class App {
     {
         return Math.sqrt(x);
     }
-
     public static long factorial(int x) {
         if (x < 0) return -1;
         long result = 1;
-        for (int i = 1; i <= x; i++) {
+        for (int i = 1; i <= x; i++) 
+        {
             result *= i;
         }
         return result;
